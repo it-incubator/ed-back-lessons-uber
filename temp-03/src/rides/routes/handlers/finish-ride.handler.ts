0a6179e20706e3a7c2c99f6c1705c923b798db94
chlamydamanada <evil.cabaret@gmail.com>
2025-03-20 20:33:37 +0300

    temp-03: fix handlers with transactions

--- temp-03/src/rides/routes/handlers/finish-ride.handler.ts
+++ temp-03/src/rides/routes/handlers/finish-ride.handler.ts
@@ -12,57 +12,49 @@ export async function finishRideHandler(
   req: Request<{ id: string }, {}, {}>,
   res: Response,
 ) {
-  try {
-    const id = req.params.id;
-    const ride = await ridesRepository.findById(id);
+  const id = req.params.id;
+  const ride = await ridesRepository.findById(id);
 
-    if (!ride) {
-      res
-        .status(HttpStatus.NotFound)
-        .send(
-          createErrorMessages([{ field: 'id', message: 'Ride not found' }]),
-        );
+  if (!ride) {
+    res
+      .status(HttpStatus.NotFound)
+      .send(createErrorMessages([{ field: 'id', message: 'Ride not found' }]));
 
-      return;
-    }
+    return;
+  }
 
-    //Если поездка уже завершена, то обновить статус ей нельзя
-    if (ride.status === RideStatus.Finished) {
-      res
-        .status(HttpStatus.BadRequest)
-        .send(
-          createErrorMessages([
-            { field: 'id', message: 'Ride is already over' },
-          ]),
-        );
-
-      return;
-    }
-    // Начало транзакции
-    const session: ClientSession = client.startSession();
-    session.startTransaction();
-
-    try {
-      await ridesRepository.updateStatus(id, RideStatus.Finished, session);
-
-      await driversRepository.updateStatus(
-        ride.driver.id,
-        DriverStatus.Online,
-        session,
+  //Если поездка уже завершена, то обновить статус ей нельзя
+  if (ride.status === RideStatus.Finished) {
+    res
+      .status(HttpStatus.BadRequest)
+      .send(
+        createErrorMessages([{ field: 'id', message: 'Ride is already over' }]),
       );
 
-      // Подтверждение транзакции
-      await session.commitTransaction();
+    return;
+  }
+  // Начало транзакции
+  const session: ClientSession = client.startSession();
+  session.startTransaction();
 
-      res.sendStatus(HttpStatus.NoContent);
-    } catch (e: unknown) {
-      // Откат транзакции в случае ошибки
-      await session.abortTransaction();
-      res.sendStatus(HttpStatus.InternalServerError);
-    } finally {
-      await session.endSession();
-    }
+  try {
+    await ridesRepository.updateStatus(id, RideStatus.Finished, session);
+
+    await driversRepository.updateStatus(
+      ride.driver.id,
+      DriverStatus.Online,
+      session,
+    );
+
+    // Подтверждение транзакции
+    await session.commitTransaction();
+
+    res.sendStatus(HttpStatus.NoContent);
   } catch (e: unknown) {
+    // Откат транзакции в случае ошибки
+    await session.abortTransaction();
     res.sendStatus(HttpStatus.InternalServerError);
+  } finally {
+    await session.endSession();
   }
 }
