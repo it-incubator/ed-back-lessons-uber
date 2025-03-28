98422d299a8fd299ea94946ac6eca517f563b11b
chlamydamanada <evil.cabaret@gmail.com>
2025-03-28 18:37:57 +0300

    fix: temp-02

--- temp-02/__tests__/e2e/rides/rides.e2e.spec.ts
+++ temp-02/__tests__/e2e/rides/rides.e2e.spec.ts
@@ -1,16 +1,14 @@
+// @ts-ignore
 import request from 'supertest';
+// @ts-ignore
 import express from 'express';
 import { setupApp } from '../../../src/setup-app';
 import { generateBasicAuthToken } from '../../utils/generate-admin-auth-token';
 import { HttpStatus } from '../../../src/core/types/http-statuses';
-import { DriverStatus } from '../../../src/drivers/types/driver';
-import { RideStatus } from '../../../src/rides/types/ride';
-import { createDriver } from '../../utils/drivers/create-driver';
 import { clearDb } from '../../utils/clear-db';
 import { createRide } from '../../utils/rides/create-ride';
 import { RIDES_PATH } from '../../../src/core/paths/paths';
 import { getRideById } from '../../utils/rides/get-ride-by-id';
-import { getDriverById } from '../../utils/drivers/get-driver-by-id';
 
 describe('Rides API', () => {
   const app = express();
@@ -23,20 +21,11 @@ describe('Rides API', () => {
   });
 
   it('✅ should create ride; POST /api/rides', async () => {
-    const driver = await createDriver(app);
-
-    const createdRide = await createRide(app, { driverId: driver.id });
-
-    expect(createdRide.status).toBe(RideStatus.InProgress);
+    await createRide(app);
   });
 
   it('✅ should return rides list; GET /api/rides', async () => {
-    const driver2 = await createDriver(app, {
-      name: 'Sam',
-      email: 'sam@example.com',
-    });
-
-    await createRide(app, { driverId: driver2.id });
+    await createRide(app);
 
     const rideListResponse = await request(app)
       .get(RIDES_PATH)
@@ -48,11 +37,7 @@ describe('Rides API', () => {
   });
 
   it('✅ should return ride by id; GET /api/rides/:id', async () => {
-    const driver = await createDriver(app);
-
-    const createdRide = await createRide(app, {
-      driverId: driver.id,
-    });
+    const createdRide = await createRide(app);
 
     const getRide = await getRideById(app, createdRide.id);
 
@@ -60,34 +45,6 @@ describe('Rides API', () => {
       ...createdRide,
       id: expect.any(Number),
       createdAt: expect.any(String),
-    });
-  });
-
-  it('✅ should finish ride; POST /api/rides/:id/actions/finish', async () => {
-    const driver = await createDriver(app);
-
-    const createdRide = await createRide(app, {
-      driverId: driver.id,
-    });
-
-    await request(app)
-      .post(`${RIDES_PATH}/${createdRide.id}/actions/finish`)
-      .set('Authorization', adminToken)
-      .expect(HttpStatus.NoContent);
-
-    const getRide = await getRideById(app, createdRide.id);
-
-    expect(getRide).toEqual({
-      ...createdRide,
-      status: RideStatus.Finished,
-      updatedAt: expect.any(String),
-    });
-
-    const getDriver = await getDriverById(app, driver.id);
-
-    expect(getDriver).toEqual({
-      ...driver,
-      status: DriverStatus.Online,
     });
   });
 });
--- temp-02/__tests__/utils/drivers/get-driver-by-id.ts
+++ temp-02/__tests__/utils/drivers/get-driver-by-id.ts
@@ -6,17 +6,14 @@ import { DRIVERS_PATH } from '../../../src/core/paths/paths';
 import { generateBasicAuthToken } from '../generate-admin-auth-token';
 import { Driver } from '../../../src/drivers/types/driver';
 
-export async function getDriverById<R = Driver>(
+export async function getDriverById(
   app: Express,
   driverId: number,
-  expectedStatus?: HttpStatus,
-): Promise<R> {
-  const testStatus = expectedStatus ?? HttpStatus.Ok;
-
+): Promise<Driver> {
   const driverResponse = await request(app)
     .get(`${DRIVERS_PATH}/${driverId}`)
     .set('Authorization', generateBasicAuthToken())
-    .expect(testStatus);
+    .expect(HttpStatus.Ok);
 
   return driverResponse.body;
 }
